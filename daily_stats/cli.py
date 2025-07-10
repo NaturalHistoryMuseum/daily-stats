9d843ccf50eb0760f905a8fb52065ff3a0d5bd63
Ginger Burns <alycejenni@gmail.com>
2025-07-10 10:01:34 +0100

    feat: add config object using env vars

--- daily_stats/cli.py
+++ daily_stats/cli.py
@@ -1,13 +1,24 @@
+import json
+
 import click
 
 from daily_stats import __version__
+from daily_stats.config import Config
 
 
 @click.group()
-def cli():
-    pass
+@click.pass_context
+def cli(ctx):
+    config = Config()
+    ctx.obj = {'config': config}
 
 
 @cli.command()
 def version():
     click.echo(__version__)
+
+
+@cli.command()
+@click.pass_context
+def get_config(ctx):
+    click.echo(json.dumps(ctx.obj['config'].as_dict(), indent=2))
