f5e0c674eda05b739cb6186f7526202a31789496
Ginger Burns <alycejenni@gmail.com>
2025-07-11 12:09:41 +0100

    fix: make columns nullable

--- daily_stats/db/alma.py
+++ daily_stats/db/alma.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 from datetime import date as dt
+from typing import Optional
 
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -12,7 +13,7 @@ class AlmaCsfPackageComp(Base):
     __tablename__ = 'alma_csf_package_comp'
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    bib_level: Mapped[str]
-    collection: Mapped[str]
-    date: Mapped[dt]
+    bib_level: Mapped[Optional[str]]
+    collection: Mapped[Optional[str]]
+    date: Mapped[Optional[dt]]
     record_count: Mapped[int] = mapped_column(default=0)
