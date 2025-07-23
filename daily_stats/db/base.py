9ff0f34d6de1ae69c29ed49fa990abce79fa127a
Ginger Burns <alycejenni@gmail.com>
2025-07-23 15:14:16 +0100

    feat: add a helper method to data models for cleaning dicts

--- daily_stats/db/base.py
+++ daily_stats/db/base.py
@@ -5,4 +5,9 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
-    pass
+    @classmethod
+    def strip(cls, data_dict):
+        """
+        Remove keys from the given data dict that don't match a table column.
+        """
+        return {k: v for k, v in data_dict.items() if k in cls.__table__.columns}
